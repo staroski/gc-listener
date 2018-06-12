cc1beb0414f0f02c1e53ac258158983d0435ba35
Ricardo Artur Staroski <ricardo.staroski@gmail.com>
2018-06-12 17:04:15 -0300

    Implemented an example of use.

---
+++ example/GcListenerExample.java
@@ -0,0 +1,59 @@
+import static java.lang.System.out;
+
+import java.lang.ref.Reference;
+import java.lang.ref.WeakReference;
+import java.util.LinkedList;
+import java.util.List;
+
+import br.com.staroski.gc.GcListener;
+import br.com.staroski.gc.GcMonitor;
+
+public class GcListenerExample {
+
+    public static void main(String[] args) {
+        try {
+            new GcListenerExample().executar();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+    private void executar() throws Exception {
+        // an listener that gets notified when an object is garbage collected
+        GcListener<Integer> gcListener = objectKey -> out.printf("object binded with key %d was garbage collected%n", objectKey);
+
+        // the garbage collection monitor
+        GcMonitor gcMonitor = GcMonitor.get();
+        gcMonitor.addGcListener(gcListener);
+
+        List<MyClass> objects = new LinkedList<>();
+        List<Reference<MyClass>> weakReferences = new LinkedList<>();
+
+        out.printf("creating objects%n");
+        for (int objectKey = 0; objectKey < 100; objectKey++) {
+            // creating an object
+            MyClass object = new MyClass();
+            objects.add(object);
+            // binding that object with a key, so that we can track the garbage collection
+            out.printf("binding object %s with key %d%n", object, objectKey);
+            WeakReference<MyClass> weakReference = gcMonitor.bind(object, objectKey);
+            weakReferences.add(weakReference);
+        }
+
+        out.printf("listing objects%n");
+        for (Reference<MyClass> weakReference : weakReferences) {
+            MyClass object = weakReference.get();
+            out.printf("%s%n", object);
+        }
+
+        out.printf("killing objects%n");
+        objects = null;
+        System.gc(); // on real life it would not be explicity called
+
+        out.printf("listing objects%n");
+        for (Reference<MyClass> weakReference : weakReferences) {
+            MyClass object = weakReference.get();
+            out.printf("%s%n", object); // will be null
+        }
+    }
+}
---
+++ example/MyClass.java
@@ -0,0 +1,21 @@
+
+public class MyClass {
+
+    private static int indexCounter;
+
+    public final int indexNumber;
+
+    public MyClass() {
+        indexNumber = indexCounter++;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        indexCounter--;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s(%d)", getClass().getSimpleName(), indexNumber);
+    }
+}
